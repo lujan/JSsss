43adf91933f508d2bfd51ca0d14f53ffa55075c4
OpenShift guest <lujan0008@gmail.com>
2013-09-18 08:04:38 -0400

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jssss::Application.config.secret_key_base = 'ebf001377a4d7be2bc0bd2a2e44c4d323fb7c975900f41db3df69d7cc9b8d6b143c4493b97d39f64495ac0d5fa3e94e019dfde24b79c855cc4c48d4406b3f11e'
